bee4e7a068980df28ea1b5ffeb90911676bef8e1
Usman-99 <usmanyousuf1137@gmail.com>
2024-10-01 18:33:49 +0500

    not completed

--- src/components/GoogleSignin.js
+++ src/components/GoogleSignin.js
@@ -1,13 +1,14 @@
 import Glogo from "../Images/googlelogo.png";
 const GoogleSignin = () => {
   return (
-    <div className="text-center mt-6">
-      <p className="text-sm text-gray-600">or </p>
-      <button className="mt-4 w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-        <img src={Glogo} alt="Google Logo" className="w-5 h-5 mr-3"></img>
-        Continue with Google
-      </button>
-    </div>
+    <></>
+    // <div className="text-center mt-6">
+    //   <p className="text-sm text-gray-600">or </p>
+    //   <button className="mt-4 w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+    //     <img src={Glogo} alt="Google Logo" className="w-5 h-5 mr-3"></img>
+    //     Continue with Google
+    //   </button>
+    // </div>
   );
 };
 export default GoogleSignin;
